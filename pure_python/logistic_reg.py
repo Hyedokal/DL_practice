73a6149371d59a7c01834c08f015bed79a1580f7
정호진 <xcvbn5414@gmail.com>
2024-08-29 17:13:16 +0900

    Logistic Regression 실습

--- pure_python/logistic_reg.py
+++ pure_python/logistic_reg.py
@@ -15,7 +15,7 @@ X = np.array([[12, 3, 6, 0],
               [24, 11, 11, 1],
               [24, 4, 5, 0]])
 
-W = np.random.randn(3, 1)
+W = np.random.randn(3, 1)/3
 B = np.random.randn()
 # 일반적인 LR인 0.01을 사용할 경우 Loss 값이 진동하게 된다.
 LEARNING_RATE = 0.001
@@ -41,6 +41,8 @@ def loss_gradient(X, Y, W, B):
     # print('='*50)
 
     # partial L / partial A(y hat)
+    print('Z: ', Z)
+    print('A: ', A)
     dL_dA = -(Y / A) + ((1-Y) / (1-A))
     # partial A / partial Z
     dA_dZ = A - A**2
@@ -86,5 +88,17 @@ if __name__ == '__main__':
     
 2. LR 값을 0.01(가장 일반적인 값)로 할 경우 Loss값이 진동하게 되는데 그 이유가 무엇일까?
 -> guess 1. 데이터 수가 너무 적어서?
--> guess 2. 첫 W, b 값을 randn으로 정해서? 
+-> guess 2. 첫 W, b 값을 randn으로 정해서?
+
+3. 아주 가끔 line 28, 44에서 각각 'divide by zero encountered in log' 오류를 뿜으며
+   loss, W, b값이 nan으로 도배된다. 언제 이런 오류가 발생하는 걸까?
+   * 발생 당시 parameter value:
+    Before W:  [[ 0.90236829]
+                [ 1.93830252]
+                [-0.0326827 ]]
+    Before B:  -0.9514057277258494
+-> 돌려봤을 때 Z 값이 너무 커서 A(=sig(Z))값이 1로 도배될 경우 발생하는 것 같다.
+-> Z = np.dot(X, W) + B인데, sig(11) = 0.9999832.., sig(15) = 0.9999996.. 이다.
+-> 즉 W값이 약 0.7정도만 돼도 sig값이 1에 수렴하는 것 같다..
+-> 그래서 W 초기화 시 '나누기 3'만 붙여 줘도 학습이 잘 진행되는 것을 볼 수 있다.
 '''
